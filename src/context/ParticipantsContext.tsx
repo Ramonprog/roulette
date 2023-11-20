bacd5072589ca4aa9e7b848b0d87bfcb2af8167b
Ramonprog <programador.ramon@gmail.com>
2023-11-20 17:27:21 -0300

    criando contexto

---
+++ src/context/ParticipantsContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState, ReactNode } from 'react';
+
+export const ParticipantsContext = createContext({})
+
+type ProviderProps = {
+    children: ReactNode;
+};
+
+const ParticipantsProvider = ({ children }: ProviderProps) => {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [participants, setParticipants] = useState([]);
+
+    return (
+        <ParticipantsContext.Provider value={{ participants, setParticipants }}>
+            {children}
+        </ParticipantsContext.Provider>
+    );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default ParticipantsProvider
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import ParticipantsProvider from './context/ParticipantsContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ParticipantsProvider>
+      <App />
+    </ParticipantsProvider>
   </React.StrictMode>,
 )
